625e340258ae02a0050ba5479d9323384adcb0ed
rom42pla <romeo.lanzino@gmail.com>
2021-03-26 23:55:19 +0100

    Fixed rolls

--- scripts/utils.py
+++ scripts/utils.py
@@ -9,9 +9,8 @@ def roll_dice(roll_string: str, return_steps: bool = False):
     roll_string = " ".join(roll_string.split()).strip()
     raw_rolls = re.findall(pattern="[0-9]* *d *[0-9]+", string=roll_string, flags=re.IGNORECASE)
     for roll in raw_rolls:
-        multiplier, dice = [int(n) for n in roll.split("d")]
-        if not multiplier:
-            multiplier = 1
+        multiplier, dice = [n.strip() for n in roll.split("d")]
+        multiplier, dice = int(multiplier) if multiplier else 1, int(dice)
         rolls = np.random.randint(low=1, high=dice + 1, size=multiplier)
         parsed_roll = f"({'+'.join([str(n) for n in rolls])})"
         parsed_roll_string = re.sub(pattern=roll, repl=parsed_roll, string=parsed_roll_string, count=1)
